c8e69977e33944ea9f6fc6bdb1a93b2e952682d9
Olena Soldatova <lena.soldatova@gmail.com>
2025-01-30 21:06:47 +0200

    fix conflicts

--- lesson2/hello.js
+++ lesson2/hello.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 console.log("Hello World!");
-
 console.log("Hello World2!");
 console.log("Hello  Olena!");
 console.log("Hello  Olena!!!!");
-=======
-console.log("Hello  Olena!");
->>>>>>> 6dfd388 (change hello.js)
