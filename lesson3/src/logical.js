335f2a744e926775858c02f7719de8b118859211
Olena Soldatova <lena.soldatova@gmail.com>
2025-02-01 18:16:20 +0200

    Fix Logical errors

--- lesson3/src/logical.js
+++ lesson3/src/logical.js
@@ -6,9 +6,9 @@ let nullValue = null;
 let undefinedValue;
 
 console.log("num == 10: ", num == 10);
-console.log("num === str: ", num === str);
-console.log("num != 5: ", num != 5);
-console.log("num !== str: ", num !== str);
+console.log("num === str: ", num === Number(str));
+console.log("num !== 5: ", num !== Number(str));
+console.log("num !== str: ", num !== Number(str));
 console.log("num > 5: ", num > 5);
 console.log("num < 20: ", num < 20);
 console.log("num >= 10: ", num >= 10);
@@ -26,9 +26,9 @@ console.log("NOT str: ", !str);
 console.log("nullValue == undefinedValue: ", nullValue == undefinedValue);
 console.log("nullValue === undefinedValue: ", nullValue === undefinedValue);
 
-console.log('10 == "10" is ', 10 == "10");
-console.log('true == 1 is ', true == num);
+console.log('10 == "10" is ', 10 == Number("10"));
+console.log('true == 1 is ', Number(true) == num);
 console.log('nullValue == undefinedValue is ', nullValue == undefinedValue);
-console.log('10 === "10" is ', 10 === "10");
-console.log('true === 1 is ', true === num);
+console.log('10 === "10" is ', 10 === Number("10"));
+console.log('true === 1 is ', Number(true) === num);
 console.log('nullValue === undefinedValue is ', nullValue === undefinedValue);
