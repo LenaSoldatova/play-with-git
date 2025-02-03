82e988286695dd53c095bdcd6ea29cdb06160f81
Olena Soldatova <lena.soldatova@gmail.com>
2025-02-03 21:59:47 +0200

    configs and fix eslint

--- lesson4/eslint.config.mjs
+++ lesson4/eslint.config.mjs
@@ -1,21 +1,47 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
+import pluginJs from "@eslint/js";
+import unicorn from "eslint-plugin-unicorn";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: {
-      globals: globals.browser,
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: {
+            globals: globals.browser
+        },
+        plugins: {
+            unicorn
+        },
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            semi: ["error", "always"],
+            indent: ["error", 4, { SwitchCase: 1 }],
+            "max-len": [
+                "warn",
+                {
+                    code: 140,
+                    ignoreUrls: true,
+                    ignoreStrings: true,
+                    ignoreTemplateLiterals: true
+                }
+            ],
+            "brace-style": ["warn"],
+            "comma-dangle": ["error", "never"],
+            "no-multiple-empty-lines": ["error", { max: 2 }],
+            "no-trailing-spaces": ["error", { skipBlankLines: false }],
+            "eol-last": ["error", "always"],
+            "no-var": ["error"],
+            "prefer-const": "error",
+            "unicorn/filename-case": [
+                "error",
+                {
+                    case: "kebabCase"
+                }
+            ]
+        }
     },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "warn",
-      "indent": ["error", 2],
-      "quotes": ["error", "double"],
-    },
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
+    js.configs.recommended,
+    ...tseslint.configs.recommended
 ];
--- lesson4/src/loop.js
+++ lesson4/src/loop.js
@@ -1,46 +1,47 @@
 let numbers = [];
-let max = 100;
+const max = 100;
 
 for (let i = 0; i < max; i++) {
-  numbers.push(i);
+    numbers.push(i);
 }
 
-for (const num of numbers) {    
-  console.log(num);
+for (const num of numbers) {
+    console.log(num);
 }
 
 let i = 0;
+
 while (i < 100) {
-  console.log(i);
-  i++;
+    console.log(i);
+    i++;
 }
 
 let j = 0;
 do {
-  if (j % 2 === 0) {
-    console.log(j);
-  }
-  j++;
+    if (j % 2 === 0) {
+        console.log(j);
+    }
+    j++;
 } while (j < 100);
 
 
 numbers = [];
 for (let i = 100; i >= 0; i -= 10) {
-  numbers.push(i);
+    numbers.push(i);
 }
 
 for (const num of numbers) {
-  console.log(num);
+    console.log(num);
 }
 
 i = 100;
 while (i >= 0) {
-  console.log(i);
-  i -= 10;
+    console.log(i);
+    i -= 10;
 }
 
 j = 100;
 do {
-  console.log(j);
-  j -= 10;
+    console.log(j);
+    j -= 10;
 } while (j >= 0);
