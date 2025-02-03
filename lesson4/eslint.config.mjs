16a22393b1f4c733603052e91c497d3fe1bfa918
Olena Soldatova <lena.soldatova@gmail.com>
2025-02-03 21:02:27 +0200

    fix eslint errors

--- lesson4/eslint.config.mjs
+++ lesson4/eslint.config.mjs
@@ -1,12 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+    },
+  },
+  js.configs.recommended,
   ...tseslint.configs.recommended,
 ];
--- lesson4/src/objects.js
+++ lesson4/src/objects.js
@@ -1,31 +1,31 @@
 const complexObject = {
-    name: "Object",
-    details: {
-        id: 1,
-        type: "sample",
-        attributes: { speed: "fast", efficiency: "efficient", reliability: "reliable" }
-    },
+  name: "Object",
+  details: {
+    id: 1,
+    type: "sample",
+    attributes: { speed: "fast", efficiency: "efficient", reliability: "reliable" }
+  },
     
-    print(indent = "") {
-        for (const [key, value] of Object.entries(this)) {
-            if (typeof value === "object" && key !== "print") { 
-                console.log(`${indent}${key}:`);
-                this.printNested(value, indent + "  ");
-            } else if (key !== "print") {
-                console.log(`${indent}${key}: ${value}`);
-            }
-        }
-    },
-    printNested(obj, indent = "") {
-        for (const [key, value] of Object.entries(obj)) {
-            if (typeof value === "object") {
-                console.log(`${indent}${key}:`);
-                this.printNested(value, indent + "  ");
-            } else {
-                console.log(`${indent}${key}: ${value}`);
-            }
-        }
+  print(indent = "") {
+    for (const [key, value] of Object.entries(this)) {
+      if (typeof value === "object" && key !== "print") { 
+        console.log(`${indent}${key}:`);
+        this.printNested(value, indent + "  ");
+      } else if (key !== "print") {
+        console.log(`${indent}${key}: ${value}`);
+      }
     }
+  },
+  printNested(obj, indent = "") {
+    for (const [key, value] of Object.entries(obj)) {
+      if (typeof value === "object") {
+        console.log(`${indent}${key}:`);
+        this.printNested(value, indent + "  ");
+      } else {
+        console.log(`${indent}${key}: ${value}`);
+      }
+    }
+  }
 };
 
 complexObject.print();
